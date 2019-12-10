92d0fb75340df2ec675fc85931bf5c9f415c7938
Andyy <andrewbergheim@gmail.com>
2019-12-10 00:25:43 -0800

    functional AJAX requests for current data

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 
-$("#search-button").on("click"){
+$("#search-button").on("click", function(){
+    // parameter variable initialization
+    let temperature;
+    let humidity;
+    let windSpeed;
+    let uvIndex;
+    //geo variable init
+    let lat;
+    let long;
     // variables for ajax request construction
-    let apiKey = 
-    let queryURL = 
+    let apiKey = "9f3acf47fedf13c0f5ba5b0b771606a7";
+    let city = $("#search-text").val();
+    let firstQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=" + apiKey
+    let secondQueryURL; // to be filled later
+    //this next function is declared here but not called until later
+    
+    function UVCall(){
+        $.ajax({
+            url: secondQueryURL,
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+            uvIndex = response.value;
+        })
+    }
     //now make the actual ajax request
     $.ajax({
-        url: queryURL ,
+        url: firstQueryURL,
         method: "GET"
     }).then(function(response){
-        //function to add ajax info onto the page
-    });
-};
+        console.log(response)
+        //record temperature data
+        temperature = response.main.temp;
+        humidity = response.main.humidity;
+        windSpeed = response.wind.speed;
+        //record geo data
+        lat = response.coord.lat;
+        console.log("lat" + lat)
+        long = response.coord.lon;
+        console.log("long" + long)
+        // url for second query using coordinates
+        secondQueryURL = "https://api.openweathermap.org/data/2.5/uvi?APPID=" + apiKey + "&lat=" + lat +"&lon=" + long;
+        console.log(secondQueryURL);
+        UVCall()
+
+    //now for the second call
+    })
+    
+});
+
